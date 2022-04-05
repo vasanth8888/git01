9adf728c55db87fb63f912176adebc91147c08c7
vasanth8888 <vasanthveeda09@gmail.com>
2022-04-05 11:48:33 +0530

    git01

---
+++ src/test/java/org/git01/Git14.java
@@ -0,0 +1,26 @@
+package org.git01;
+
+public class Git14 {
+	
+	
+	
+	
+	
+	private void long1() {
+		// TODO Auto-generated method stub
+   System.out.println(1);   
+	}
+	
+	
+   private void long2() {
+	// TODO Auto-generated method stub
+	   System.out.println(2);
+}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
